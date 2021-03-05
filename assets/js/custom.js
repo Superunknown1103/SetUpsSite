cdb881446b41fbc79db3a6e5cf472f828e6d78c9
Bruce <superunknown1103@gmail.com>
2021-03-04 21:09:02 -0600

    kingdom single

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,11 @@
 let note = document.getElementById('fa-music');
-
+let single = document.getElementById('single');
 var i = 0;
 function change() {
   var color = ["black", "blue", "brown", "green", "yellow", "orange", "red", "purple", "violet", "white"];
   note.style.color = color[i];
+  single.style.color = color[i];
+
   i = (i + 1) % color.length;
 }
 setInterval(change, 1000);
